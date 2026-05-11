6f5f9f182da7db8425d303b27dea728da4fc640e
jpmazierooo <jpmazinhooo@gmail.com>
2026-05-11 20:30:36 -0300

    feat: cria classe Mensagem para tratamento de erros

---
+++ src/main/java/view/Mensagem.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package view;
+
+public class Mensagem extends Exception {
+
+    public Mensagem(String mensagem) {
+        super(mensagem);
+    }
+}

022bc96a59eccb644421c06b2630fe1e9b3e457a
JoaoPedroMarimon <marimonjoaopedro@gmail.com>
2026-05-11 19:46:07 -0300

    feat: Criação da tabela produto

--- banco.sql
+++ banco.sql
@@ -2,9 +2,22 @@ CREATE DATABASE IF NOT EXISTS sistema_estoque;
 
 USE sistema_estoque;
 
+-- Criação da tabela produto
 CREATE TABLE categoria (
     id INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(100) NOT NULL,
     tamanho ENUM('PEQUENO', 'MEDIO', 'GRANDE') NOT NULL,
     embalagem ENUM('LATA', 'VIDRO', 'PLASTICO') NOT NULL
+);
+
+-- Criação da tabela produto
+CREATE TABLE IF NOT EXISTS produto (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(100) NOT NULL,
+    preco_unitario DECIMAL(10,2) NOT NULL,
+    unidade VARCHAR(20) NOT NULL,
+    quantidade_estoque INT NOT NULL,
+    quantidade_minima INT NOT NULL,
+    quantidade_maxima INT NOT NULL,
+    categoria VARCHAR(100) NOT NULL
 );
